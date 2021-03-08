e89e278aa2fc60279866940eea0e9c856533a086
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-08 13:38:19 +0600

    10 introduction to axios advance data loading system added

--- src/component/TopNews/TopNews.js
+++ src/component/TopNews/TopNews.js
@@ -1,13 +1,22 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Bootstarp from '../Bootstarp';
 
 const TopNews = () => {
     const [articles,setArticles] = useState([]);
+    // useEffect(()=>{
+    //     const url ='https://newsapi.org/v2/top-headlines?country=us&apiKey=a3f24ac276824971b4f56f5ef295d72f'
+    //     fetch(url)
+    //     .then(res => res.json())
+    //     .then(data =>setArticles(data.articles))
+    // },[])
+
+
+    // fetch API er alternative "axios" eta fetch API theke ar o valo boro project er somoy valo kaje lage
     useEffect(()=>{
         const url ='https://newsapi.org/v2/top-headlines?country=us&apiKey=a3f24ac276824971b4f56f5ef295d72f'
-        fetch(url)
-        .then(res => res.json())
-        .then(data =>setArticles(data.articles))
+        axios(url)
+        .then(data =>setArticles(data.data.articles))
     },[])
     return (
         <div>
